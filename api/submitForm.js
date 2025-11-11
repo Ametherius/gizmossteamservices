5118b2bd80a8ffeedd797e290f3eeefc1f2f43f6
Rylan Deacon-Rogers <rylan.deaconrogers@hotmail.com>
2025-11-11 09:07:54 -0800

    Change Email Style

--- api/submitForm.js
+++ api/submitForm.js
@@ -1,134 +1,66 @@
-import nodemailer from 'nodemailer';
+import nodemailer from "nodemailer";
 
 export default async function handler(req, res) {
+  if (req.method !== "POST") {
+    res.statusCode = 405;
+    return res.end("Method not allowed");
+  }
+  if (!req.body) {
+    res.statusCode = 400;
+    return res.end("Missing request body");
+  }
 
-    if (req.method !== 'POST') {
-        res.statusCode = 405;
-        return res.end('Method not allowed');
+  const {
+    firstName,
+    lastName,
+    email,
+    phone,
+    service,
+    propertyType,
+    squareFootage,
+    preferredDate,
+    preferredTime,
+    additionalInfo,
+  } = req.body;
+
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    },
+  });
+
+  const htmlContent = `
+    <p style="font-family: 'Arial', sans-serif; padding: 20px; border: 1px solid #000000; margin: 0; text-align: start;"><strong>Name: </strong>${firstName} ${lastName}</p>
+    <p style="font-family: 'Arial', sans-serif; padding: 20px; border: 1px solid #000000; margin: 0; text-align: start;"><strong>Email: </strong>${email}</p>
+    <p style="font-family: 'Arial', sans-serif; padding: 20px; border: 1px solid #000000; margin: 0; text-align: start;"><strong>Phone: </strong>${phone}</p>
+    <p style="font-family: 'Arial', sans-serif; padding: 20px; border: 1px solid #000000; margin: 0; text-align: start;"><strong>Service: </strong>${service}</p>
+    <p style="font-family: 'Arial', sans-serif; padding: 20px; border: 1px solid #000000; margin: 0; text-align: start;"><strong>Property Type: </strong>${propertyType}</p>
+    <p style="font-family: 'Arial', sans-serif; padding: 20px; border: 1px solid #000000; margin: 0; text-align: start;"><strong>Square Footage: </strong>${squareFootage}</p>
+    <p style="font-family: 'Arial', sans-serif; padding: 20px; border: 1px solid #000000; margin: 0; text-align: start;"><strong>Preferred Date: </strong>${preferredDate}</p>
+    <p style="font-family: 'Arial', sans-serif; padding: 20px; border: 1px solid #000000; margin: 0; text-align: start;"><strong>Preferred Time: </strong>${preferredTime}</p>
+    <p style="font-family: 'Arial', sans-serif; padding: 20px; border: 1px solid #000000; margin: 0; text-align: start;"><strong>Additional Information: </strong>${additionalInfo}</p>
+    `;
+
+  const mailOptions = {
+    from: `Gizmo's Steam Services <${process.env.EMAIL_USER}>`,
+    to: process.env.EMAIL_USER,
+    subject: "New Quote Request",
+    html: htmlContent,
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.error(error);
+      res.statusCode = 500;
+      return res.json({ success: false, message: "Error sending email" });
+    } else {
+      res.statusCode = 200;
+      return res.json({
+        success: true,
+        message: "Quote request sent successfully!",
+      });
     }
-    if (!req.body) {
-        res.statusCode = 400;
-        return res.end('Missing request body');
-    }
-
-    const { firstName, lastName, email, phone, service, propertyType, squareFootage, preferredDate, preferredTime, additionalInfo } = req.body;
-
-
-    const transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-            user: process.env.EMAIL_USER,
-            pass: process.env.EMAIL_PASS,
-        },
-    });
-
-    const htmlContent = `
-    <table width="100%" cellspacing="0" cellpadding="0">
-        <tr>
-            <td align="center">
-                <!-- Outer Container -->
-                <table width="600px" cellspacing="0" cellpadding="0" border="2" style="background-color: #ca8831; border-radius: 10px; overflow: hidden;">
-                    <!-- header -->
-                    <tr>
-                        <td align="center">
-                            <table width="100%" cellspacing="0" cellpadding="0" border="0">
-                                <tr>
-                                    <td align="center">
-                                        <h1 style="font-family: 'Arial', sans-serif; color: #000000; font-size: 24px; font-weight: bold; text-decoration: underline; padding: 10px;">New Quote Request</h1>
-                                    </td>
-                                </tr>
-                                <!-- Body -->
-                                 <tr>
-                                    <td align="center">
-                                        <h4 style="font-family: 'Arial', sans-serif; color: #000000; font-size: 16px; font-weight: bold; text-align: center;text-transform: uppercase;">Personal Information</h4>
-                                    </td>
-                                 </tr>
-
-                                 <tr>
-                                    <td style="padding: 20px;">
-                                        <h3 style="font-family: 'Arial', sans-serif; color: #000000; font-size: 16px; font-weight: bold; text-transform: uppercase;">NAME:</h3>
-                                        <p style="font-family: 'Arial', sans-serif; color: #000000; font-size: 16px; font-weight: bold; margin: 0;">${firstName} ${lastName}</p>
-                                        <td style="padding: 20px;">
-                                            <h3 style="font-family: 'Arial', sans-serif; color: #000000; font-size: 16px; font-weight: bold; text-transform: uppercase;">EMAIL:</h3>
-                                            <p style="font-family: 'Arial', sans-serif; color: #000000; font-size: 16px; font-weight: bold; margin: 0;">${email}</p>
-                                        </td>
-                                    </td>
-                                 </tr>
-                                 <tr>
-                                    
-                                 </tr>
-                                 <tr>
-                                    <td style="padding: 20px;">
-                                        <h3 style="font-family: 'Arial', sans-serif; color: #000000; font-size: 16px; font-weight: bold; text-transform: uppercase;">PHONE:</h3>
-                                        <p style="font-family: 'Arial', sans-serif; color: #000000; font-size: 16px; font-weight: bold; margin: 0;">${phone}</p>
-                                    </td>
-                                 </tr>
-
-                                 <tr>
-                                    <td align="center">
-                                        <h4 style="font-family: 'Arial', sans-serif; color: #000000; font-size: 16px; font-weight: bold; text-align: center;text-transform: uppercase;">Service Details</h4>
-                                    </td>
-                                 </tr>
-                                 <tr>
-                                    <td style="padding: 20px;">
-                                        <h3 style="font-family: 'Arial', sans-serif; color: #000000; font-size: 16px; font-weight: bold; text-transform: uppercase;">Service:</h3>
-                                        <p style="font-family: 'Arial', sans-serif; color: #000000; font-size: 16px; font-weight: bold; margin: 0;">${service}</p>
-                                        <td>
-                                        <h3 style="font-family: 'Arial', sans-serif; color: #000000; font-size: 16px; font-weight: bold; text-transform: uppercase;">Property Type:</h3>
-                                        <p style="font-family: 'Arial', sans-serif; color: #000000; font-size: 16px; font-weight: bold; margin: 0;">${propertyType}</p>
-                                        </td>
-                                    </td>
-                                 </tr>
-                                 <tr>
-                                    <td style="padding: 20px;">
-                                        <h3 style="font-family: 'Arial', sans-serif; color: #000000; font-size: 16px; font-weight: bold; text-transform: uppercase;">Approximat Square Feet:</h3>
-                                        <p style="font-family: 'Arial', sans-serif; color: #000000; font-size: 16px; font-weight: bold; margin: 0;">${squareFootage}</p>
-                                    </td>
-                                   
-                                 </tr>
-                                 <tr>
-                                    <td style="padding: 20px;">
-                                        <h3 style="font-family: 'Arial', sans-serif; color: #000000; font-size: 16px; font-weight: bold; text-transform: uppercase;">Preffered Date:</h3>
-                                        <p style="font-family: 'Arial', sans-serif; color: #000000; font-size: 16px; font-weight: bold; margin: 0;">${preferredDate}</p>
-                                        <td>
-                                            <h3 style="font-family: 'Arial', sans-serif; color: #000000; font-size: 16px; font-weight: bold; text-transform: uppercase;">Preffered Time:</h3>
-                                        <p style="font-family: 'Arial', sans-serif; color: #000000; font-size: 16px; font-weight: bold; margin: 0;">${preferredTime}</p>
-                                        </td>
-                                    </td>
-                                 </tr>
-                                 <tr>
-                                        <td style="padding: 20px;">
-                                        
-                                    </td>
-                                 </tr>
-                                 <tr>
-                                    <td style="padding: 20px;">
-                                        <h3 style="font-family: 'Arial', sans-serif; color: #000000; font-size: 16px; font-weight: bold; text-transform: uppercase;">Additional Information:</h3>
-                                        <p style="font-family: 'Arial', sans-serif; color: #000000; font-size: 16px; font-weight: bold; margin: 0;">${additionalInfo}</p>
-                                    </td>
-                                 </tr>
-                            </table>
-                        </td>
-                    </tr>
-  </table>
-    `
-    
-    
-    const mailOptions = {
-        from: `Gizmo's Steam Services <${process.env.EMAIL_USER}>`,
-        to: process.env.EMAIL_USER,
-        subject: 'New Quote Request',
-        html: htmlContent,
-    };
-    
-    transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-            console.error(error);
-            res.statusCode = 500;
-            return res.json({ success: false, message: 'Error sending email' });
-        } else {
-            res.statusCode = 200;
-            return res.json({ success: true, message: 'Quote request sent successfully!' });
-        }
-    });
+  });
 }
--- api/submitReview.js
+++ api/submitReview.js
@@ -1,96 +1,47 @@
-import nodemailer from 'nodemailer';
+import nodemailer from "nodemailer";
 
 export default async function handler(req, res) {
-    if (req.method !== 'POST') {
-        res.statusCode = 405;
-        return res.end('Method not allowed');
+  if (req.method !== "POST") {
+    res.statusCode = 405;
+    return res.end("Method not allowed");
+  }
+  if (!req.body) {
+    res.statusCode = 400;
+    return res.end("Missing request body");
+  }
+
+  const { name, email, rating, review } = req.body;
+
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    },
+  });
+
+  const htmlContent = `
+    <p style="font-family: 'Arial', sans-serif; padding: 20px; border: 1px solid #000000; margin: 0; text-align: start;"><strong>Name: </strong>${name}</p>
+  <p style="font-family: 'Arial', sans-serif; padding: 20px; border: 1px solid #000000; margin: 0; text-align: start;"><strong>Email: </strong>${email}</p>
+  <p style="font-family: 'Arial', sans-serif; padding: 20px; border: 1px solid #000000; margin: 0; text-align: start;"><strong>Rating: </strong>${rating}</p>
+  <p style="font-family: 'Arial', sans-serif; padding: 20px; border: 1px solid #000000; margin: 0; text-align: start;"><strong>Review: </strong>${review}</p>
+    `;
+
+  const mailOptions = {
+    from: `Gizmo's Steam Services <${process.env.EMAIL_USER}>`,
+    to: process.env.EMAIL_USER,
+    subject: "New Review",
+    html: htmlContent,
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.error(error);
+      res.statusCode = 500;
+      return res.json({ success: false, message: "Error sending email" });
+    } else {
+      res.statusCode = 200;
+      return res.json({ success: true, message: "Review sent successfully!" });
     }
-    if (!req.body) {
-        res.statusCode = 400;
-        return res.end('Missing request body');
-    }
-
-    const { name, email, rating, review } = req.body;
-
-    const transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-            user: process.env.EMAIL_USER,
-            pass: process.env.EMAIL_PASS,
-        },
-    });
-
-    const htmlContent = `
-    <table width="100%" cellspacing="0" cellpadding="0">
-        <tr>
-            <td align="center">
-                <!-- Outer Container -->
-                <table width="600px" cellspacing="0" cellpadding="0" border="2" style="background-color: #ca8831; border-radius: 10px; overflow: hidden;">
-                    <!-- header -->
-                    <tr>
-                        <td align="center">
-                            <table width="100%" cellspacing="0" cellpadding="0" border="0">
-                                <tr>
-                                    <td align="center">
-                                        <h1 style="font-family: 'Arial', sans-serif; color: #000000; font-size: 24px; font-weight: bold; text-decoration: underline; padding: 10px;">New Review</h1>
-                                    </td>
-                                </tr>
-                                <!-- Body -->
-                                <tr>
-                                    <td align="center">
-                                        <h4 style="font-family: 'Arial', sans-serif; color: #000000; font-size: 16px; font-weight: bold; text-align: center;text-transform: uppercase;">Review Information</h4>
-                                    </td>
-                                </tr>
-                                
-                                <tr>
-                                    <td style="padding: 20px;">
-                                        <h3 style="font-family: 'Arial', sans-serif; color: #000000; font-size: 16px; font-weight: bold; text-transform: uppercase;">NAME:</h3>
-                                        <p style="font-family: 'Arial', sans-serif; color: #000000; font-size: 16px; font-weight: bold; margin: 0;">${name}</p>
-                                    </td>
-                                </tr>
-                                
-                                <tr>
-                                    <td style="padding: 20px;">
-                                        <h3 style="font-family: 'Arial', sans-serif; color: #000000; font-size: 16px; font-weight: bold; text-transform: uppercase;">EMAIL:</h3>
-                                        <p style="font-family: 'Arial', sans-serif; color: #000000; font-size: 16px; font-weight: bold; margin: 0;">${email}</p>
-                                    </td>
-                                </tr>
-                                
-                                <tr>
-                                    <td style="padding: 20px;">
-                                        <h3 style="font-family: 'Arial', sans-serif; color: #000000; font-size: 16px; font-weight: bold; text-transform: uppercase;">RATING:</h3>
-                                        <p style="font-family: 'Arial', sans-serif; color: #000000; font-size: 16px; font-weight: bold; margin: 0;">${rating}</p>
-                                    </td>
-                                </tr>
-                                
-                                <tr>
-                                    <td style="padding: 20px;">
-                                        <h3 style="font-family: 'Arial', sans-serif; color: #000000; font-size: 16px; font-weight: bold; text-transform: uppercase;">REVIEW:</h3>
-                                        <p style="font-family: 'Arial', sans-serif; color: #000000; font-size: 16px; font-weight: bold; margin: 0;">${review}</p>
-                                    </td>
-                                </tr>
-                            </table>
-                        </td>
-                    </tr>
-                </table>
-    `
-
-    const mailOptions = {
-        from: `Gizmo's Steam Services <${process.env.EMAIL_USER}>`,
-        to: process.env.EMAIL_USER,
-        subject: 'New Review',
-        html: htmlContent,
-    };
-    
-    
-    transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-            console.error(error);
-            res.statusCode = 500;
-            return res.json({ success: false, message: 'Error sending email' });
-        } else {
-            res.statusCode = 200;
-            return res.json({ success: true, message: 'Review sent successfully!' });
-        }
-    });
+  });
 }
