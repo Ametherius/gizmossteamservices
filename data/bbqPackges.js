6f2d7df82b9dfc82e4fa4c2ce994f5d547403ced
Ametherius <139707007+Ametherius@users.noreply.github.com>
2026-04-24 06:33:27 -0700

    Fix BBQ Pricing

--- data/bbqPackges.js
+++ data/bbqPackges.js
@@ -4,14 +4,17 @@ export const bbqPackages = [
     icon: "fa-solid fa-leaf fs-1 mb-4",
     style: "card bbq-card",
     price: 199,
-    tagline: "Ideal for a nice refresh",
+    tagline:
+      "Perfect for regular maintenance, giving your BBQ a fresh clean, improved appearance, and keeping it ready for your next cookout.",
     list: ["Light Clean", "Grates + basic interior wipe", "Exterior Wipe"],
   },
   {
     title: "The Deep Clean (4+ Hours)",
     icon: "fa-solid fa-dumbbell fs-1 mb-4",
-    tagline: "Restores heat performance & reduces flare-ups",
+    tagline:
+      "Cuts through built-up grease and grime to improve heat performance, promote even cooking, and help prevent unwanted flare-ups.",
     price: 289,
+    style: "card bbq-card",
     list: [
       "Full Degrease (Steam)",
       "Grates, heat plates, burners",
@@ -25,6 +28,7 @@ export const bbqPackages = [
     tagline:
       "Brings heavily neglected BBQ's back to life with intensive restoration, stronger heat performance, and safer, more even grilling",
     price: 349,
+    style: "card bbq-card",
     list: [
       "Steam Deep Clean",
       "Heavy grease removal",
